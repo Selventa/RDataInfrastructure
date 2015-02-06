5f86f0ea86b05f1b0fb0b024615bb976c082ae6b
Lindsey Osimiri <losimiri@selventa.com>
2015-02-06 16:34:24 -0500

    made limma code more modular

--- R/limma_esets.R
+++ R/limma_esets.R
@@ -1,41 +1,71 @@
 library(limma)
 
-GEOPipelineLimma <- function(from.raw.num = 1, eset.list = esets){
+GEOPipelineLimma <- function(relev.eset){
   #This function will run the appropriate limma functions on an expression set list generated through the GEO pipeline.
-  #typeof(from.raw.num) == integer/double. This gives the index in the eset.list where the expressionset of interest is 
-  #located.
-  #typeof(eset.list) == list. This should be a list of eSet items. The eset of interest should have a specially modified
-  #pData attribute. This modification should be made through an UpdateAnnotations script.
+  #typeof(relev.eset) == expressionSet. This should be the eset of interest. 
+  #If using LimmaGeneric, read the following:
   #One column in pData should start with DESIGN_ preceding the column name. This indicates that this column holds the
   #information that is the reason for these contrasts (disease type, tissue type, etc).
   #One or more columns in pData should start with CONTRAST_.These should have 1's indicating which rows are included 
   #in the numerator of the contrast and -1's indicating which rows are included in the denominator of that contrast. 
   #There should only be one contrast marked per row.
   
-  #locate the design column
-  design.col <- colnames(pData(eset.list[[from.raw.num]]))[grepl("DESIGN_", colnames(pData(eset.list[[from.raw.num]])))]
-  stopifnot(length(design.col)==1)
-  #locate columns which delineate contrasts
-  contrast.cols <- colnames(pData(eset.list[[from.raw.num]]))[grepl("CONTRAST_", colnames(pData(eset.list[[from.raw.num]])))]
-  #create model matrix and linear fit for this eset
-  design <- eval(parse(text = paste0("model.matrix(~0+", design.col,", pData(eset.list[[", from.raw.num, "]]))", sep="")))
-  fit <- lmFit(eset.list[[from.raw.num]], design)
-  eFits <- list()
-
-  #run each contrast and save eBayes fit and top differential probesets to a list
-  for (contrast in 1:length(contrast.cols)){
-    contrast.matrix <- matrix(0, nrow=ncol(design), ncol=1, dimnames=list(colnames(design), "Contrast Value"))
-    contrast.numerator <- pData(eset.list[[from.raw.num]])[pData(eset.list[[from.raw.num]])[,contrast.cols[contrast]]==1, design.col]
-    stopifnot(length(unique(contrast.numerator)) == 1)
-    contrast.denominator <- pData(eset.list[[from.raw.num]])[pData(eset.list[[from.raw.num]])[,contrast.cols[contrast]]== -1, design.col]
-    stopifnot(length(unique(contrast.denominator)) == 1)
-    contrast.matrix[unique(contrast.numerator), 1] <- 1
-    contrast.matrix[unique(contrast.denominator), 1] <- -1
-    tmp.fit <- contrasts.fit(fit, contrast.matrix)
-    tmp.eFit <- eBayes(tmp.fit)
-    eFits[[contrast]] <- tmp.eFit
-    names(eFits)[contrast] <- contrast.cols[contrast]
-  }
+  #check if there is a specific limma function for this dataset
+  relev.eset.limma.func <- paste0("Limma_", notes(relev.eset)$name)
+  if (exists(relev.eset.limma.func)) {
+    limma.func.name <- relev.eset.limma.func
+  }else{limma.func.name <- "LimmaGeneric"}
+  eFits <- eval(parse(text=paste0(limma.func.name,"(relev.eset)")))
   return(eFits)
 }
-eFits <- GEOPipelineLimma()
+
+LimmaGeneric <- function(relev.eset){
+  #typeof(relev.eset) == expressionSet. This is the eset of interest.
+  
+  #locate design column
+  design.col <- grep("^DESIGN_", colnames(pData(relev.eset)), value=TRUE)
+  stopifnot(length(design.col)==1)
+  #locate columns which delineate contrasts
+  contrast.cols <- grep("^CONTRAST_", colnames(pData(relev.eset)), value=TRUE)
+  #create model matrix and linear fit for this eset
+  design <- eval(parse(text = paste0("model.matrix(~0+", design.col,", pData(relev.eset))", sep="")))
+  fit <- lmFit(relev.eset, design)
+  eFit.list <- list()
+  
+  #run each contrast using RunContrasts; save eBayes fit to a list
+  for (contrast in 1:length(contrast.cols)){
+    contrast.eFit <- tryCatch({
+      RunContrasts(contrast.cols[contrast], design.col, design, relev.eset)
+    }, error = function(err){
+      print(paste("Error:", err))
+      print(paste("An error occurred. Contrast ", contrast.cols[contrast], "will not be calculated."))
+      return(err)
+    })
+    eFit.list[[contrast]] <- contrast.eFit
+    names(eFit.list)[contrast] <- contrast.cols[contrast]
+  }
+  return(eFit.list)
+}
+
+RunContrasts <- function(contrast.column, design.column, design.mat, relev.eset){
+  #typeof(contrast.column) == string. This is the name of the column that holds the designation for which samples should
+  #be compared in the contrast.
+  #typeof(design.column) == string. This is the name of the column that holds the relevant design information (see docstring
+  #for GEOPipelineLimma for more information).
+  #typeof(design.mat) == matrix. This is the model/design matrix for the linear fit model for the eset of interest.
+  #typeof(relev.eset) == expressionSet. This is the eset of interest.
+  
+  contrast.matrix <- matrix(0, nrow=ncol(design.mat), ncol=1, dimnames=list(colnames(design.mat), "Contrast Value"))
+  contrast.numerator <- pData(relev.eset)[pData(relev.eset)[,contrast.column]==1, design.column]
+  #ensures that there is no user error in selecting the samples for contrast; they should all be of the same type in the
+  #design column
+  stopifnot(length(unique(contrast.numerator)) == 1)
+  contrast.denominator <- pData(relev.eset)[pData(relev.eset)[,contrast.column]== -1, design.column]
+  stopifnot(length(unique(contrast.denominator)) == 1)
+  contrast.matrix[unique(contrast.numerator), 1] <- 1
+  contrast.matrix[unique(contrast.denominator), 1] <- -1
+  tmp.fit <- contrasts.fit(fit, contrast.matrix)
+  tmp.eFit <- eBayes(tmp.fit)
+  return(tmp.eFit)
+}
+eFits <- GEOPipelineLimma(esets[[1]])
